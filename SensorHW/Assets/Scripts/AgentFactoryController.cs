d94d92b5aa7891aa89332e5f5f0366fd72395138
P. Crozier <pcrozier@knights.ucf.edu>
2015-02-13 17:48:23 -0500

    I added the "click to create agent" functionality. Clicking creates the static agents. The scene starts with 1 static and 1 non-static agent. How the non-static agent moves needs to be changed, though, since it is incorrect (at least I think so based off the instructions).

---
+++ SensorHW/Assets/Scripts/AgentFactoryController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AgentFactoryController : MonoBehaviour {
+	public GameObject agent3Prefab;
+	private bool makeAgent = false;
+	private Vector3 creationCoordinates;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Fire1")) {
+			creationCoordinates = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			creationCoordinates.z = 0;
+			makeAgent = true;
+		} else if (makeAgent) {
+			// Don't make the agent until we release the fire button.
+			// Otherwise, we'll spawn a ton on top of eachother everytime we click.
+			makeAgent = false;
+			createAgent (creationCoordinates);
+		}
+	}
+
+	void createAgent(Vector3 creationCoordinates){
+		Instantiate (agent3Prefab, creationCoordinates, Quaternion.identity);
+	}
+}
